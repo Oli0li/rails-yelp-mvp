77acb21fceda96f5363a86b1dab4bd31ad1b571f
Olivia Bonnal <oliviabonnal@gmail.com>
2022-09-28 10:36:58 +0100

    added condition to show a 'no reviews' message when there are no reviews yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "restaurants#index"
   resources :restaurants, except: [:edit, :update, :destroy] do
     resources :reviews, only: :create
   end
